8c3e151ce2e80122ac2041eec10143e839ecbd12
Jahir124 <jahirmp13@gmail.com>
2025-06-13 18:50:31 -0500

    Add token recognition log and fix token handling

--- analizadores_dart/analizador_lexico.py
+++ analizadores_dart/analizador_lexico.py
@@ -1,14 +1,5 @@
 import ply.lex as lex
 
-# Lista de tokens
-tokens = [
-    'ID', 'FLOAT', 'INT', 'PLUS', 'MINUS', 'TIMES', 'DIVIDE', 'MOD',
-    'EQ', 'NEQ', 'LT', 'LE', 'GT', 'GE', 'ASSIGN',
-    'LPAREN', 'RPAREN', 'LBRACE', 'RBRACE', 'LBRACKET', 'RBRACKET',
-    'SEMI', 'COMMA', 'AND', 'OR', 'NOT', 'BITNOT', 'DOT', 'INCREMENT', 'DECREMENT', 'ARROW', 'IDIV', 'COLON',
-    'PLUSEQ', 'MINUSEQ', 'TIMESEQ', 'DIVEQ', 'MODEQ'
-]
-
 reserved = {
     "String": "STRING",
     "double": "DOUBLE",
@@ -39,54 +30,96 @@ reserved = {
     "readLineSync": "READLINESYNC"
 }
 
-tokens += list(reserved.values())
+# Lista de tokens
+tokens = (
+    'ID',
+    'FLOAT',
+    'PLUS',
+    'MINUS',
+    'TIMES',
+    'DIVIDE',
+    'MODULE',
+    'EQUALS',
+    'NOT_EQUALS',
+    'LESS',
+    'LESS_EQUAL',
+    'GREATER',
+    'GREATER_EQUAL',
+    'ASSIGN',
+    'LPAREN',
+    'RPAREN',
+    'LBRACE',
+    'RBRACE',
+    'LBRACKET',
+    'RBRACKET',
+    'SEMICOLON',
+    'DOUBLE_COLON',
+    'QUESTION',
+    'COMMA',
+    'AND',
+    'OR',
+    'NOT',
+    'BITNOT',
+    'DOT',
+    'INCREMENT',
+    'DECREMENT',
+    'ARROW',
+    'INT_DIVIDE',
+    'COLON',
+    'PLUS_EQUAL',
+    'MINUS_EQUAL',
+    'TIMES_EQUAL',
+    'DIVIDE_EQUAL',
+    'MODULE_EQUAL'
+) + tuple(reserved.values())
 
 
 # Reglas de expresiones regulares
-t_PLUS     = r'\+'
-t_MINUS    = r'-'
-t_TIMES    = r'\*'
-t_DIVIDE   = r'/'
-t_MOD      = r'%'
-t_EQ       = r'=='
-t_NEQ      = r'!='
-t_LT       = r'<'
-t_LE       = r'<='
-t_GT       = r'>'
-t_GE       = r'>='
-t_ASSIGN   = r'='
-t_PLUSEQ   = r'\+='
-t_MINUSEQ  = r'-='
-t_TIMESEQ  = r'\*='
-t_DIVEQ    = r'/='
-t_MODEQ    = r'%='
-t_LPAREN   = r'\('
-t_RPAREN   = r'\)'
-t_LBRACE   = r'\{'
-t_RBRACE   = r'\}'
-t_LBRACKET = r'\['
-t_RBRACKET = r'\]'
-t_SEMI     = r';'
-t_COMMA    = r','
-t_AND      = r'&&'
-t_OR       = r'\|\|'
-t_NOT      = r'!'
-t_BITNOT = r'~'
-t_DOT = r'\.'
-t_INCREMENT = r'\+\+'
-t_DECREMENT = r'--'
-t_ARROW    = r'=>'
-t_IDIV     = r'~/'
-t_COLON = r':'
-
+t_PLUS            = r'\+'
+t_MINUS           = r'-'
+t_TIMES           = r'\*'
+t_INT_DIVIDE      = r'~/'
+t_DIVIDE          = r'/'
+t_MODULE          = r'%'
+t_EQUALS          = r'=='
+t_NOT_EQUALS      = r'!='
+t_LESS            = r'<'
+t_LESS_EQUAL      = r'<='
+t_GREATER         = r'>'
+t_GREATER_EQUAL   = r'>='
+t_ASSIGN          = r'='
+t_PLUS_EQUAL      = r'\+='
+t_MINUS_EQUAL     = r'-='
+t_TIMES_EQUAL     = r'\*='
+t_DIVIDE_EQUAL    = r'/='
+t_MODULE_EQUAL    = r'%='
+t_LPAREN          = r'\('
+t_RPAREN          = r'\)'
+t_LBRACE          = r'\{'
+t_RBRACE          = r'\}'
+t_LBRACKET        = r'\['
+t_RBRACKET        = r'\]'
+t_SEMICOLON       = r';'
+t_COMMA           = r','
+t_AND             = r'&&'
+t_OR              = r'\|\|'
+t_NOT             = r'!'
+t_BITNOT          = r'~'
+t_DOT             = r'\.'
+t_INCREMENT       = r'\+\+'
+t_DECREMENT       = r'--'
+t_ARROW           = r'=>'
+t_COLON           = r':'
+t_DOUBLE_COLON    = r'::'
+t_QUESTION        = r'\?'
 
 # Ignorar espacios y tabulaciones
 t_ignore = ' \t'
 
 # Comentarios
-def t_COMMENT(t):
+def t_COMMENT_SINGLE(t):
     r'//.*'
-    pass 
+    pass
 
 def t_COMMENT_MULTI(t):
     r'/\*[\s\S]*?\*/'
@@ -122,68 +155,90 @@ def t_error(t):
     print(f"[Error Léxico] Línea {t.lineno}: Carácter ilegal '{t.value[0]}'")
     t.lexer.skip(1)
 
+#Construcción del lexer
 lexer = lex.lex()
 
-# Prueba rápida
+# Prueba rápida Algoritmo Jahir
 if __name__ == "__main__":
     data = '''
-        // Algoritmo de búsqueda con estadísticas básicas 
-        class Analizador { 
-        List<int> datos; 
-        Map<String, dynamic> resumen; 
-        Analizador(this.datos) { 
-        resumen = {"min": 0, "max": 0, "promedio": 0.0, "existe": 
-        false}; 
-        } 
-        int busquedaBinaria(int objetivo) { 
-        int ini = 0, fin = datos.length - 1; 
-        while (ini <= fin) { 
-        int mid = (ini + fin) ~/ 2; 
-        if (datos[mid] == objetivo) { 
-        resumen["existe"] = true; 
-        return mid; 
-        } else if (datos[mid] < objetivo) { 
-        ini = mid + 1; 
-        } else { 
-        fin = mid - 1; 
-        } 
-        } 
-        return -1; 
-        } 
-        void calcularResumen() { 
-        int suma = 0, min = datos[0], max = datos[0]; 
-        for (int val in datos) { 
-        suma += val; 
-        if (val < min) min = val; 
-        if (val > max) max = val; 
-        } 
-        resumen["min"] = min; 
-        resumen["max"] = max; 
-        resumen["promedio"] = suma / datos.length; 
-        } 
-        void mostrarResumen() { 
-        print("Resumen:"); 
-        print("Min: ${resumen["min"]}, Max: ${resumen["max"]}, 
-        Promedio: ${resumen["promedio"]}"); 
-        print("Elemento buscado existe: ${resumen["existe"]}"); 
-        } 
-        } 
-        void main() { 
-        List<int> lista = [3, 6, 8, 12, 15, 20]; 
-        int objetivo = 12; 
-        var analizador = Analizador(lista); 
-        int pos = analizador.busquedaBinaria(objetivo); 
-        analizador.calcularResumen(); 
-        if (pos != -1) { 
-        print("Elemento encontrado en posición $pos"); 
-        } else { 
-        print("Elemento no encontrado"); 
-        } 
-        analizador.mostrarResumen(); 
-        var entrada = 2.34;
+        int binarySearch(List<int> listIntegers, int target) {
+         int start = 0;
+         int end = listIntegers.length - 1;
+         while (start <= end) {
+         int half = (start + end) ~/ 2; // ~/ It's integer division
+         if (listIntegers[half] == target) {
+         return half; // Found, return the index
+         } else if (listIntegers[medio] < target) {
+         start = half + 1;
+         } else {
+         end = half - 1;
+         }
+         }
+         return -1; // Not found
         }
-
     '''
-    lexer.input(data)
-    for tok in lexer:
-        print(tok)
+
+#Prueba rápida Algoritmo Javier
+if __name__ == "__main__":
+    data2 = '''
+            // Algoritmo de búsqueda con estadísticas básicas 
+            class Analizador { 
+            List<int> datos; 
+            Map<String, dynamic> resumen; 
+            Analizador(this.datos) { 
+            resumen = {"min": 0, "max": 0, "promedio": 0.0, "existe": 
+            false}; 
+            } 
+            int busquedaBinaria(int objetivo) { 
+            int ini = 0, fin = datos.length - 1; 
+            while (ini <= fin) { 
+            int mid = (ini + fin) ~/ 2; 
+            if (datos[mid] == objetivo) { 
+            resumen["existe"] = true; 
+            return mid; 
+            } else if (datos[mid] < objetivo) { 
+            ini = mid + 1; 
+            } else { 
+            fin = mid - 1; 
+            } 
+            } 
+            return -1; 
+            } 
+            void calcularResumen() { 
+            int suma = 0, min = datos[0], max = datos[0]; 
+            for (int val in datos) { 
+            suma += val; 
+            if (val < min) min = val; 
+            if (val > max) max = val; 
+            } 
+            resumen["min"] = min; 
+            resumen["max"] = max; 
+            resumen["promedio"] = suma / datos.length; 
+            } 
+            void mostrarResumen() { 
+            print("Resumen:"); 
+            print("Min: ${resumen["min"]}, Max: ${resumen["max"]}, 
+            Promedio: ${resumen["promedio"]}"); 
+            print("Elemento buscado existe: ${resumen["existe"]}"); 
+            } 
+            } 
+            void main() { 
+            List<int> lista = [3, 6, 8, 12, 15, 20]; 
+            int objetivo = 12; 
+            var analizador = Analizador(lista); 
+            int pos = analizador.busquedaBinaria(objetivo); 
+            analizador.calcularResumen(); 
+            if (pos != -1) { 
+            print("Elemento encontrado en posición $pos"); 
+            } else { 
+            print("Elemento no encontrado"); 
+            } 
+            analizador.mostrarResumen(); 
+            var entrada = 2.34;
+            }
+
+        '''
+
+lexer.input(data)
+for tok in lexer:
+    print(tok)
