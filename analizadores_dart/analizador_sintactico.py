c404accfde2dad7e47771b9ca0981caacdede825
javierkiu <javier28420@gmail.com>
2025-06-20 10:40:01 -0500

    reglas para llamados a funciones, return, while, incrementos/decrementos, y clases

--- analizadores_dart/analizador_sintactico.py
+++ analizadores_dart/analizador_sintactico.py
@@ -7,57 +7,72 @@ def p_compiler(p):
 
 def p_statement_composed(p):
     '''statement_composed : statement
-                      | statement_composed statement'''
+                          | statement_composed statement'''
 
 def p_statement(p):
-    '''statement : print
+    '''statement : print_stmt
                  | control_structures
                  | function
                  | list_def
                  | variable_def
-                 | SEMICOLON'''
+                 | SEMICOLON
+                 | function_call
+                 | return_statement
+                 | incdec_statement
+                 | class_def'''
 
 def p_variable_def(p):
     '''variable_def : type ID ASSIGN expression SEMICOLON
-                           | DYNAMIC ID ASSIGN expression SEMICOLON
-                           | VAR ID ASSIGN expression SEMICOLON
-                           | STATIC type ID ASSIGN expression SEMICOLON
-                           | STATIC VAR ID ASSIGN expression SEMICOLON
-                           | STATIC DYNAMIC ID ASSIGN expression SEMICOLON'''
+                    | DYNAMIC ID ASSIGN expression SEMICOLON
+                    | VAR ID ASSIGN expression SEMICOLON
+                    | STATIC type ID ASSIGN expression SEMICOLON
+                    | STATIC VAR ID ASSIGN expression SEMICOLON
+                    | STATIC DYNAMIC ID ASSIGN expression SEMICOLON'''
 
 # Print
-def p_print(p):
-    '''print : PRINT LPAREN RPAREN SEMICOLON
-             | PRINT LPAREN value RPAREN SEMICOLON
-             | PRINT LPAREN expression RPAREN SEMICOLON'''
+def p_print_stmt(p):
+    '''print_stmt : PRINT LPAREN RPAREN SEMICOLON
+                  | PRINT LPAREN value RPAREN SEMICOLON
+                  | PRINT LPAREN expression RPAREN SEMICOLON'''
 
 # Expressions
 def p_expression_arithmetic(p):
     '''expression : expression PLUS expression
                   | expression MINUS expression
                   | expression TIMES expression
-                  | expression DIVIDE expression'''
+                  | expression DIVIDE expression
+                  | expression INT_DIVIDE expression
+                  | expression MODULE expression'''
 
-def p_expression_simplified(p):
-    '''expression : value PLUS value'''
-
+def p_expression_paren(p):
+    '''expression : LPAREN expression RPAREN'''
 
 def p_expression_value(p):
     '''expression : value'''
 
+def p_expression_incdec(p):
+    '''expression : ID INCREMENT
+                  | ID DECREMENT
+                  | INCREMENT ID
+                  | DECREMENT ID'''
+
+def p_incdec_statement(p):
+    '''incdec_statement : ID INCREMENT SEMICOLON
+                        | ID DECREMENT SEMICOLON
+                        | INCREMENT ID SEMICOLON
+                        | DECREMENT ID SEMICOLON'''
 
 # Conditionals
 def p_control_structures(p):
     '''control_structures : if_block
-                          | if_block else_block'''
+                          | if_block else_block
+                          | while_loop'''
 
 def p_if_block(p):
-    '''if_block : IF LPAREN conditions RPAREN LBRACKET statement_composed RBRACKET'''
-
+    '''if_block : IF LPAREN conditions RPAREN LBRACE statement_composed RBRACE'''
 
 def p_else_block(p):
-    '''else_block : ELSE LBRACKET statement_composed RBRACKET'''
-
+    '''else_block : ELSE LBRACE statement_composed RBRACE'''
 
 def p_conditions(p):
     '''conditions : condition
@@ -70,25 +85,50 @@ def p_condition(p):
 
 def p_comparison_operator(p):
     '''comparison_operator : GREATER
-                  | LESS
-                  | GREATER_EQUAL
-                  | LESS_EQUAL
-                  | EQUALS'''
+                           | LESS
+                           | GREATER_EQUAL
+                           | LESS_EQUAL
+                           | EQUALS
+                           | NOT_EQUALS'''
 
 # Functions
 def p_function(p):
-    '''function : type ID LPAREN parameters RPAREN LBRACKET statement_composed RBRACKET
-                | VOID ID LPAREN parameters RPAREN LBRACKET statement_composed RBRACKET
-                | type ID LPAREN RPAREN LBRACKET statement_composed RBRACKET
-                | VOID ID LPAREN RPAREN LBRACKET statement_composed RBRACKET'''
+    '''function : type ID LPAREN parameters RPAREN LBRACE statement_composed RBRACE
+                | VOID ID LPAREN parameters RPAREN LBRACE statement_composed RBRACE
+                | type ID LPAREN RPAREN LBRACE statement_composed RBRACE
+                | VOID ID LPAREN RPAREN LBRACE statement_composed RBRACE'''
 
 def p_parameters(p):
     '''parameters : parameter
-                      | parameters COMMA parameter'''
+                  | parameters COMMA parameter'''
 
 def p_parameter(p):
     '''parameter : type ID
                  | REQUIRED type ID'''
+
+def p_function_call(p):
+    '''function_call : ID LPAREN RPAREN SEMICOLON
+                     | ID LPAREN argument_list RPAREN SEMICOLON'''
+
+def p_argument_list(p):
+    '''argument_list : expression
+                     | argument_list COMMA expression'''
+
+def p_return_statement(p):
+    '''return_statement : RETURN expression SEMICOLON
+                       | RETURN SEMICOLON'''
+
+def p_while_loop(p):
+    '''while_loop : WHILE LPAREN conditions RPAREN LBRACE statement_composed RBRACE'''
+
+def p_class_body(p):
+    '''class_body : statement_composed'''
+
+def p_class_def(p):
+    '''class_def : CLASS ID LBRACE class_body RBRACE
+                 | CLASS ID EXTENDS ID LBRACE class_body RBRACE
+                 | CLASS ID IMPLEMENTS ID LBRACE class_body RBRACE
+                 | CLASS ID EXTENDS ID IMPLEMENTS ID LBRACE class_body RBRACE'''
 
 #Data Types
 def p_type(p):
@@ -100,7 +140,7 @@ def p_type(p):
 # Lists
 def p_list_def(p):
     '''list_def : LIST LBRACKET value_list RBRACKET SEMICOLON
-                       | LIST LESS type GREATER ID ASSIGN LBRACKET value_list RBRACKET SEMICOLON'''
+                | LIST LESS type GREATER ID ASSIGN LBRACKET value_list RBRACKET SEMICOLON'''
 
 def p_value_list(p):
     '''value_list : value
@@ -111,10 +151,8 @@ def p_value(p):
     '''value : INT
              | FLOAT
              | STRING
-             | ID'''
-
-def p_value_bool(p):
-    '''value : TRUE
+             | ID
+             | TRUE
              | FALSE'''
 
 sintactic_results = []
@@ -139,6 +177,19 @@ def test_parser(input_code):
 
 test_parser("""
     int v = 10;
+    v++;
+    ++v;
+    v--;
+    --v;
 """)
 
-#fin Aporte Jahir Cajas
+while True:
+   try:
+       s = input('calc > ')
+   except EOFError:
+       break
+   if not s: continue
+   result = parser.parse(s)
+   print(result)
+
+#fin Aporte Jahir
